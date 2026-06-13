c1ebdf4aa0dec3bf00c547ae38608042e07a42e0
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-13 00:12:15 -0400

    Create linkedin_data_science_skills.sql

---
+++ easy/linkedin_data_science_skills.sql
@@ -0,0 +1,10 @@
+-- Problem: LinkedIn Data Science Skills
+-- Platform: DataLemur
+-- Link: https://datalemur.com/questions/matching-skills
+
+SELECT candidate_id
+FROM candidates
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(skill) = 3
+ORDER BY candidate_id
