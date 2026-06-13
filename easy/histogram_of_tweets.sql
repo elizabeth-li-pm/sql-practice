ab47c8cbd6f43cd8c917c0bd5b10f9ff40b44b3c
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-12 21:00:10 -0400

    Create histogram_of_tweets.sql

---
+++ easy/histogram_of_tweets.sql
@@ -0,0 +1,17 @@
+-- Problem: Histogram of Tweets
+-- Platform: DataLemur
+-- Difficulty: Easy
+-- Link: https://datalemur.com/questions/sql-histogram-tweets
+
+SELECT
+    tweets_posted AS tweet_bucket,
+    COUNT(user_id) AS users_num
+FROM (SELECT
+    user_id,
+    COUNT(msg) AS tweets_posted
+    FROM tweets
+    WHERE YEAR(tweet_date) = 2022
+    GROUP BY user_id
+) AS t
+GROUP BY tweets_posted
+ORDER BY tweet_bucket
