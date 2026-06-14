7d85f7d490873334aa1197b8257a29ead4291505
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-13 23:09:07 -0400

    Create facebook_average_post_hiatus.sql

---
+++ easy/facebook_average_post_hiatus.sql
@@ -0,0 +1,11 @@
+-- Problem: Average Post Hiatus (Part 1)
+-- Platform: DataLemur
+-- Link: https://datalemur.com/questions/sql-average-post-hiatus-1
+
+SELECT
+    user_id,
+    DATEDIFF(MAX(post_date), MIN(post_date)) AS days_between
+FROM posts
+WHERE YEAR(post_date) = 2021
+GROUP BY user_id
+HAVING COUNT(user_id) >= 2
