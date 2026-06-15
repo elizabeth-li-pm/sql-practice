7fc13387add3b5f4d6dba7443301ad0d9703bb8b
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-15 00:05:08 -0400

    Update microsoft_teams_power_users.sql

--- easy/microsoft_teams_power_users.sql
+++ easy/microsoft_teams_power_users.sql
@@ -2,12 +2,13 @@
 -- Platform: DataLemur
 -- Link: https://datalemur.com/questions/teams-power-users
 
-SELECT
-    sender_id,
-    COUNT(message_id) AS message_count
+SELECT 
+  sender_id,
+  COUNT(message_id) AS message_count
 FROM messages
-WHERE MONTH(sent_date) = 8
-    AND YEAR(sent_date) = 2022
+WHERE 
+  EXTRACT(YEAR FROM sent_date) = 2022 AND
+  EXTRACT(MONTH FROM sent_date) = 8
 GROUP BY sender_id
 ORDER BY message_count DESC
 LIMIT 2
