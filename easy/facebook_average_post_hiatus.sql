383e10c369cb46c1109999cd580af9254f8d4453
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-14 23:54:46 -0400

    Update facebook_average_post_hiatus.sql

--- easy/facebook_average_post_hiatus.sql
+++ easy/facebook_average_post_hiatus.sql
@@ -3,8 +3,8 @@
 -- Link: https://datalemur.com/questions/sql-average-post-hiatus-1
 
 SELECT
-    user_id,
-    DATEDIFF(MAX(post_date), MIN(post_date)) AS days_between
+  user_id,
+  DATEDIFF(MAX(post_date),MIN(post_date)) AS days_between
 FROM posts
 WHERE YEAR(post_date) = 2021
 GROUP BY user_id
