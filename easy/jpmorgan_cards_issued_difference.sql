5aed12cdbd7245b8d618b702f4c005c6d11a4944
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-16 15:23:41 -0400

    Create jpmorgan_cards_issued_difference.sql

---
+++ easy/jpmorgan_cards_issued_difference.sql
@@ -0,0 +1,9 @@
+-- Problem: Cards Issued Difference
+-- Platform: DataLemur
+-- Link: https://datalemur.com/questions/cards-issued-difference
+
+SELECT card_name,
+    MAX(issued_amount) - MIN(issued_amount) AS difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY difference DESC
