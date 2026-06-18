c05a529886724a35a85278ccd813b9946347c5cb
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-18 01:07:26 -0400

    Update tiktok_signup_confirmation_rate.sql

--- medium/tiktok_signup_confirmation_rate.sql
+++ medium/tiktok_signup_confirmation_rate.sql
@@ -2,10 +2,9 @@
 -- Platform: DataLemur
 -- Link: https://datalemur.com/questions/signup-confirmation-rate
 
-SELECT
-  ROUND(COUNT(texts.email_id)::DECIMAL
-    / COUNT(DISTINCT emails.email_id), 2) AS activation_rate
-FROM emails
-LEFT JOIN texts
-  ON emails.email_id = texts.email_id
-  AND texts.signup_action = 'Confirmed'
+SELECT 
+  ROUND(COUNT(t.email_id)
+  / COUNT(DISTINCT e.email_id)::DECIMAL , 2) AS activation_rate
+FROM emails e 
+LEFT JOIN texts t ON e.email_id = t.email_id
+    AND t.signup_action = 'Confirmed'
