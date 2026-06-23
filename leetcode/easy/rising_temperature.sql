8043b1466808b506f456de47a815ae32010848c7
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-23 03:57:23 -0400

    Create rising_temperature.sql

---
+++ leetcode/easy/rising_temperature.sql
@@ -0,0 +1,9 @@
+-- Problem: Rising Temperature
+-- Platform: LeetCode
+-- Link: https://leetcode.com/problems/rising-temperature
+
+SELECT w1.id
+FROM Weather w1
+JOIN Weather w2 ON
+    w1.recordDate = w2.recordDate + interval 1 day
+WHERE w1.temperature > w2.temperature
