23ceb4408cdd2ae14fdc44c0921bafce905b3a92
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-23 02:48:52 -0400

    Create big_countries.sql

---
+++ leetcode/easy/big_countries.sql
@@ -0,0 +1,10 @@
+-- Problem: Big Countries
+-- Platform: LeetCode
+-- Link: https://leetcode.com/problems/big-countries
+
+SELECT name,
+    population,
+    area
+FROM World
+WHERE area >= 3000000 OR
+    population >= 25000000;
