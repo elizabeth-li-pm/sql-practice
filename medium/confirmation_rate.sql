cd0f7790b037e9e873590726c7beb2f8f5da17ca
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-26 15:08:24 -0400

    Update confirmation_rate.sql

--- medium/confirmation_rate.sql
+++ medium/confirmation_rate.sql
@@ -1,11 +1,1 @@
--- Problem: Confirmation Rate
--- Platform: LeetCode
--- Link: https://leetcode.com/problems/confirmation-rate
 
-SELECT s.user_id,
-    ROUND(
-        AVG(IF(action = 'confirmed', 1, 0)), 2
-    ) AS confirmation_rate
-FROM Signups AS s
-LEFT JOIN Confirmations AS c USING (user_id)
-GROUP BY s.user_id
