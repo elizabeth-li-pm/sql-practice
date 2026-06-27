720abc60eb76bf3b3e0e123ae494c5ac01beabcc
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-27 13:00:04 -0400

    Create classes_with_at_least_5_students.sql

---
+++ leetcode/easy/classes_with_at_least_5_students.sql
@@ -0,0 +1,8 @@
+-- Problem: Classes With at Least 5 Students
+-- Platform: LeetCode
+-- Link: https://leetcode.com/problems/classes-with-at-least-5-students
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) > 4
