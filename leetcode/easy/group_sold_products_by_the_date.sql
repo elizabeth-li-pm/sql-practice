a39cdee22975216d7d46a2fd9010524e02ab9053
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-27 18:55:59 -0400

    Create group_sold_products_by_the_date.sql

---
+++ leetcode/easy/group_sold_products_by_the_date.sql
@@ -0,0 +1,10 @@
+-- Problem: Group Sold Products By The Date
+-- Platform: LeetCode
+-- Link: https://leetcode.com/problems/group-sold-products-by-the-date
+
+SELECT sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    STRING_AGG(DISTINCT product, ',' ORDER BY product) AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date
