21a607d84c81dc1cc0d365e030c00c622fbe9775
Elizabeth Li <elizabethli.career@gmail.com>
2026-06-27 14:39:16 -0400

    Create triangle_judgement.sql

---
+++ leetcode/easy/triangle_judgement.sql
@@ -0,0 +1,7 @@
+-- Problem: Triangle Judgement
+-- Platform: LeetCode
+-- Link: https://leetcode.com/problems/triangle-judgement
+
+SELECT x, y, z,
+    (CASE WHEN x + y > z AND x + z > y AND y + z > x THEN 'Yes' ELSE 'No' END) AS triangle
+FROM Triangle
